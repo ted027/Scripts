73130c3ce0f727d53a9dfbaa058d944d97df2d50
ted027 <imaitt0207@gmail.com>
2018-11-18 00:27:41 +0900

    wip

--- python_/delete_cache.py
+++ python_/delete_cache.py
@@ -2,6 +2,12 @@ import os
 import shutil
 import glob
 
+def find_all_files(directory):
+    for root, dirs, files in os.walk(directory):
+        yield root
+        for file in files:
+            yield os.path.join(root, file)
+
 file_list = glob.glob('./*__pycache__*') + glob.glob('./*.pytest_cache*')
 print(f'file_list: {file_list}')
 
@@ -10,4 +16,5 @@ print(f'dir_list: {dir_list}')
 
 for dr in dir_list:
     print(f'remopve {dr}')
-    shutil.rmtree(dr, ignore_errors=True)
+    # shutil.rmtree(dr, ignore_errors=True)
+    # delete
